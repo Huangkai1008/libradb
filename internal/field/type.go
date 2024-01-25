87bcbf253752c8ff9c35308cf6bcf1abddf5dbad
huangkai <h1770360848@outlook.com>
2024-01-25 17:14:23 +0800

    feat(field): nullable field type

--- internal/field/type.go
+++ internal/field/type.go
@@ -4,26 +4,40 @@ type TypeID int
 
 const (
 	INTEGER TypeID = iota + 1
-	BIGINT
-	FLOAT
-	CHAR
 	VARCHAR
 	BOOLEAN
+	FLOAT
 )
 
 type Type interface {
 	// TypeID returns the type id of the field.
 	TypeID() TypeID
-	// AllowNull returns whether the field allows null value.
-	AllowNull() bool
 	// ByteSize returns the byte size of the field.
 	ByteSize() int
+	Validator
+	Nullable
 }
 
-type Option[T Type] func(T)
+type Validator interface {
+	Validate()
+}
+
+type Nullable interface {
+	AllowNull() bool
+	setAllowNull(bool)
+}
+
+type Option[T any] func(T)
 
 func applyOptions[T Type](t T, options ...Option[T]) {
 	for _, option := range options {
 		option(t)
+	}
+	t.Validate()
+}
+
+func WithAllowNull[T Nullable](allowNull bool) Option[T] {
+	return func(t T) {
+		t.setAllowNull(allowNull)
 	}
 }
