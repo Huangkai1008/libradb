1207a4bc80051a61dd6d466db6f81b7e1dd1c17f
huangkai <h1770360848@outlook.com>
2024-04-19 19:59:44 +0800

    feat(memory): buffer manager interface

---
+++ internal/storage/memory/buffer_manager.go
@@ -0,0 +1,25 @@
+package memory
+
+import (
+	"io"
+
+	"github.com/Huangkai1008/libradb/internal/storage/page"
+	"github.com/Huangkai1008/libradb/internal/storage/table"
+)
+
+// BufferManager is the interface between the files/index manager and the disk manager.
+//
+// The buffer manager is responsible for managing pages in memory and
+// processing page requests from the file and index manager.
+// The buffer manager is responsible for the eviction policy,
+// or choosing which pages to evict when space is filled up.
+// When pages are evicted from memory or new pages are read in to memory (ApplyNewPage),
+// the buffer manager communicates with the disk space manager to perform the required disk operations.
+type BufferManager interface {
+	// ApplyNewPage reads a page from disk and applies it to memory.
+	ApplyNewPage(spaceID table.SpaceID) (page.Page, error)
+	// FetchPage fetches the specified page.
+	FetchPage(spaceID table.SpaceID, pageNumber page.Number) (page.Page, error)
+
+	io.Closer
+}
