2ebffb26af3ebd656cb9bfece5b7112e55a24b91
huangkai <h1770360848@outlook.com>
2024-07-30 15:38:43 +0800

    test(tree): tree delete

--- internal/storage/index/bplustree/tree_test.go
+++ internal/storage/index/bplustree/tree_test.go
@@ -3,6 +3,7 @@ package bplustree_test
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2" //nolint:revive  // ginkgo
@@ -57,6 +58,14 @@ func (m *dummyBufferManager) Close() error {
 	return nil
 }
 
+func (m *dummyBufferManager) String() string {
+	var buffer strings.Builder
+	for k, v := range m.pageMap {
+		buffer.WriteString(fmt.Sprintf("%s:%v ;", k, v))
+	}
+	return buffer.String()
+}
+
 var _ = Describe("B+ Tree Index", Ordered, func() {
 	var schema *table.Schema
 	var bufferManager memory.BufferManager
@@ -76,89 +85,280 @@ var _ = Describe("B+ Tree Index", Ordered, func() {
 
 	BeforeEach(func() {
 		bufferManager = newDummyBufferManager()
-		tree, _ = bplustree.NewBPlusTree(&bplustree.Metadata{
-			Order:  1,
-			Schema: schema,
-		}, bufferManager)
+		DeferCleanup(bufferManager.Close)
 	})
 
-	AfterEach(func() {
-		_ = bufferManager.Close()
+	Describe("Put in B+ Tree", func() {
+		BeforeEach(func() {
+			tree, _ = bplustree.NewBPlusTree(&bplustree.Metadata{
+				Order:  1,
+				Schema: schema,
+			}, bufferManager)
+		})
+		DescribeTable("Put a key in tree",
+			func(key int, values []any) {
+				record := page.NewRecordFromLiteral(values...)
+				err := tree.Put(field.NewValue(pkType, key), record)
+				Expect(err).ToNot(HaveOccurred())
+			},
+			EntryDescription("put %d with value %v"),
+			Entry(nil, 4, []any{4, "Alice", 20, true, 90.5}),
+			Entry(nil, 9, []any{9, "Bob", 21, false, 85.5}),
+			Entry(nil, 6, []any{6, "Charlie", 22, true, 80.5}),
+			Entry(nil, 2, []any{2, "David", 23, false, 75.5}),
+			Entry(nil, 10000, []any{10000, "Rita", 28, true, 68.5}),
+			Entry(nil, 1, []any{1, "Adam", 23, false, 95.5}),
+		)
+
+		When("Put duplicate key", func() {
+			It("should raise error", func() {
+				By("Add a key")
+				record := page.NewRecordFromLiteral(4, "Alice", 20, true, 90.5)
+				err := tree.Put(field.NewValue(pkType, 4), record)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Add another key")
+				record = page.NewRecordFromLiteral(9, "Bob", 21, false, 85.5)
+				err = tree.Put(field.NewValue(pkType, 9), record)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Add duplicate key")
+				record = page.NewRecordFromLiteral(4, "Alice", 20, true, 90.5)
+				err = tree.Put(field.NewValue(pkType, 4), record)
+				Expect(err).Should(MatchError(bplustree.ErrKeyExists))
+			})
+		})
 	})
 
-	Describe("Put and Get in B+ Tree", func() {
-		It("should put and get records without error", func() {
-			records := []struct {
-				key           int
-				value         *page.Record
-				treeStructure string
-			}{
-				{4, page.NewRecordFromLiteral(4, "Alice", 20, true, 90.5), `
-				// (4)
-				`},
-				{9, page.NewRecordFromLiteral(9, "Bob", 21, false, 85.5), `
-				// (4, 9)
-				`},
-				{6, page.NewRecordFromLiteral(6, "Charlie", 22, true, 80.5), `
-				//   (6)
-				//  /   \
-				// (4) (6 9)
-				`},
-				{2, page.NewRecordFromLiteral(2, "David", 23, false, 75.5), `
-				//     (6)
-				//    /   \
-				// (2 4) (6 9)
-				`},
-				{7, page.NewRecordFromLiteral(7, "Eve", 24, true, 70.5), `
-				//      (6 7)
-				//     /  |  \
-				// (2 4) (6) (7 9)
-				`},
-				{8, page.NewRecordFromLiteral(8, "Frank", 25, false, 65.5), `
-				//         (7)
-				//        /   \
-				//     (6)     (8)
-				//    /   \   /   \
-				// (2 4) (6) (7) (8 9)
-				`},
-				{3, page.NewRecordFromLiteral(3, "Grace", 26, true, 60.5), `
-				//            (7)
-				//           /   \
-				//     (3 6)       (8)
-				//   /   |   \    /   \
-				// (2) (3 4) (6) (7) (8 9)
-				`},
-				{5, page.NewRecordFromLiteral(5, "Hank", 27, true, 80.5), `
-				//            (4 7)
-				//           /  |  \
-				//   (3)      (6)       (8)
-				//  /   \    /   \    /   \
-				// (2) (3) (4 5) (6) (7) (8 9)
-				`},
-				{1, page.NewRecordFromLiteral(1, "Smith", 68, true, 78), `
-				//            (4 7)
-				//           /  |  \
-				//    (3)      (6)       (8)
-				//   /   \    /   \    /   \
-				// (1 2) (3) (4 5) (6) (7) (8 9)
-				`},
+	Describe("Get in B+ tree", func() {
+		BeforeEach(func() {
+			tree, _ = bplustree.NewBPlusTree(&bplustree.Metadata{
+				Order:  1,
+				Schema: schema,
+			}, bufferManager)
+		})
+
+		DescribeTable("Get a key in tree",
+			func(key int, values []any) {
+				By("Put a key in tree")
+				record := page.NewRecordFromLiteral(values...)
+				err := tree.Put(field.NewValue(pkType, key), record)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Get a key in tree")
+				record, err = tree.Get(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(record).To(Equal(page.NewRecordFromLiteral(values...)))
+			},
+			EntryDescription("get %d with value %v"),
+			Entry(nil, 4, []any{4, "Alice", 20, true, 90.5}),
+			Entry(nil, 9, []any{9, "Bob", 21, false, 85.5}),
+			Entry(nil, 6, []any{6, "Charlie", 22, true, 80.5}),
+			Entry(nil, 2, []any{2, "David", 23, false, 75.5}),
+			Entry(nil, 10000, []any{10000, "Rita", 28, true, 68.5}),
+			Entry(nil, 1, []any{1, "Adam", 23, false, 95.5}),
+		)
+	})
+
+	Describe("Delete in B+ tree", func() {
+		BeforeEach(func() {
+			tree, _ = bplustree.NewBPlusTree(&bplustree.Metadata{
+				Order:  1,
+				Schema: schema,
+			}, bufferManager)
+		})
+
+		DescribeTable("Delete a key in tree",
+			func(key int, values []any) {
+				By("Put a key in tree")
+				record := page.NewRecordFromLiteral(values...)
+				err := tree.Put(field.NewValue(pkType, key), record)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Delete the key in tree")
+				err = tree.Delete(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Get the key in tree")
+				record, err = tree.Get(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(record).To(BeNil())
+			},
+			EntryDescription("remove %d with value %v"),
+			Entry(nil, 4, []any{4, "Alice", 20, true, 90.5}),
+			Entry(nil, 9, []any{9, "Bob", 21, false, 85.5}),
+			Entry(nil, 6, []any{6, "Charlie", 22, true, 80.5}),
+			Entry(nil, 2, []any{2, "David", 23, false, 75.5}),
+			Entry(nil, 10000, []any{10000, "Rita", 28, true, 68.5}),
+			Entry(nil, 1, []any{1, "Adam", 23, false, 95.5}),
+		)
+
+		When("Delete non-existing key in tree", func() {
+			It("should do nothing", func() {
+				By("Put a key in tree")
+				record := page.NewRecordFromLiteral()
+				err := tree.Put(field.NewValue(pkType, 4), record)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("Delete the keys in tree")
+				for i := 0; i < 5; i++ {
+					err = tree.Delete(field.NewValue(pkType, 4))
+					Expect(err).ToNot(HaveOccurred())
+				}
+			})
+		})
+	})
+
+	Describe("WhiteBox test", func() {
+
+		BeforeEach(func() {
+			tree, _ = bplustree.NewBPlusTree(&bplustree.Metadata{
+				Order:  1,
+				Schema: schema,
+			}, bufferManager)
+		})
+
+		It("should succeed", func() {
+			By("Puts and gets in tree")
+			putBehavior := func(key int, values []any) {
+				By(fmt.Sprintf("Put %d in tree", key))
+				err := tree.Put(
+					field.NewValue(pkType, key),
+					page.NewRecordFromLiteral(values...),
+				)
+				Expect(err).ToNot(HaveOccurred())
+
+				By(fmt.Sprintf("Get %d in tree", key))
+				retrievedRecord, err := tree.Get(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(retrievedRecord).To(Equal(page.NewRecordFromLiteral(values...)))
+				GinkgoWriter.Println(tree)
 			}
 
-			for _, rec := range records {
-				By(fmt.Sprintf("Inserting key %d", rec.key), func() {
-					err := tree.Put(field.NewValue(pkType, rec.key), rec.value)
-					Expect(err).ToNot(HaveOccurred())
-					GinkgoWriter.Println(rec.treeStructure)
-					GinkgoWriter.Println(tree)
-				})
+			// (4)
+			putBehavior(4, []any{4, "Alice", 20, true, 90.5})
 
-				By(fmt.Sprintf("Getting key %d", rec.key), func() {
-					storedRecord, err := tree.Get(field.NewValue(pkType, rec.key))
-					Expect(err).ToNot(HaveOccurred())
-					Expect(storedRecord).To(Equal(rec.value))
-					GinkgoWriter.Println(tree)
-				})
+			// (4, 9)
+			putBehavior(9, []any{9, "Bob", 21, false, 85.5})
+
+			//   (6)
+			//  /   \
+			// (4) (6 9)
+			putBehavior(6, []any{6, "Charlie", 22, true, 80.5})
+
+			//     (6)
+			//    /   \
+			// (2 4) (6 9)
+			putBehavior(2, []any{2, "David", 23, false, 75.5})
+
+			//      (6 7)
+			//     /  |  \
+			// (2 4) (6) (7 9)
+			putBehavior(7, []any{7, "Eve", 24, true, 70.5})
+
+			//         (7)
+			//        /   \
+			//     (6)     (8)
+			//    /   \   /   \
+			// (2 4) (6) (7) (8 9)
+			putBehavior(8, []any{8, "Frank", 25, false, 65.5})
+
+			//            (7)
+			//           /   \
+			//     (3 6)       (8)
+			//   /   |   \    /   \
+			// (2) (3 4) (6) (7) (8 9)
+			putBehavior(3, []any{3, "Grace", 26, true, 60.5})
+
+			//           (4 7)
+			//          /  |  \
+			//   (3)      (6)       (8)
+			//  /   \    /   \    /   \
+			// (2) (3) (4 5) (6) (7) (8 9)
+			putBehavior(5, []any{5, "Hank", 27, true, 80.5})
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (1 2) (3) (4 5) (6) (7) (8 9)
+			putBehavior(1, []any{1, "Smith", 68, true, 78})
+
+			By("Deletes and gets in tree")
+			deleteBehavior := func(key int) {
+				By(fmt.Sprintf("Delete %d in tree", key))
+				err := tree.Delete(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+
+				By(fmt.Sprintf("Get %d in tree", key))
+				retrievedRecord, err := tree.Get(field.NewValue(pkType, key))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(retrievedRecord).To(BeNil())
+				GinkgoWriter.Println(tree)
 			}
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (  2) (3) (4 5) (6) (7) (8 9)
+			deleteBehavior(1)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (  2) (3) (4 5) (6) (7) (8  )
+			deleteBehavior(9)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (  2) (3) (4 5) ( ) (7) (8  )
+			deleteBehavior(6)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (  2) (3) (  5) ( ) (7) (8  )
+			deleteBehavior(4)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (   ) (3) (  5) ( ) (7) (8  )
+			deleteBehavior(2)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (   ) (3) (   ) ( ) (7) (8  )
+			deleteBehavior(5)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (   ) (3) (   ) ( ) ( ) (8  )
+			deleteBehavior(7)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (   ) ( ) (   ) ( ) ( ) (8  )
+			deleteBehavior(3)
+
+			//            (4 7)
+			//           /  |  \
+			//    (3)      (6)       (8)
+			//   /   \    /   \    /   \
+			// (   ) ( ) (   ) ( ) ( ) (   )
+			deleteBehavior(8)
 		})
 	})
 })
